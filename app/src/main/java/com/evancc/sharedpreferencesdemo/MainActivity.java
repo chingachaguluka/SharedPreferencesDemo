7554bba0b0abdf9a90a375ddb510b27a2bd2b992
Chinga Chaguluka <chinga@evancc.com>
2018-02-12 23:03:14 +0200

    String value called name stored in shared preferences with value of Chinga

--- app/src/main/java/com/evancc/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/evancc/sharedpreferencesdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.evancc.sharedpreferencesdemo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textEdit);
+
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.evancc.sharedpreferencesdemo", Context.MODE_PRIVATE);
+        //sharedPreferences.edit().putString("name", "Chinga").apply();
+
+        String name = sharedPreferences.getString("name", "Non Supplied");
+        textView.setText(name);
     }
 }
