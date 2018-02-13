9203f0b9cfb091f604a7698b16d5c06a543c3343
Chinga Chaguluka <chinga@evancc.com>
2018-02-13 11:35:09 +0200

    Add menu with Help and Settings options

--- app/src/main/java/com/evancc/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/evancc/sharedpreferencesdemo/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +19,33 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textEdit);
 
-
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.evancc.sharedpreferencesdemo", Context.MODE_PRIVATE);
         //sharedPreferences.edit().putString("name", "Chinga").apply();
 
         String name = sharedPreferences.getString("name", "Non Supplied");
         textView.setText(name);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.help:
+                Toast.makeText(this, "Help Selected", Toast.LENGTH_SHORT).show();
+                return  true;
+            default:
+                return false;
+        }
     }
 }
